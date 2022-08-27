d3dac7cf77f172e8205f3d9a5891371e2a93ee65
Chafik H'nini <chafik.hnini@gmail.com>
2022-08-27 14:00:13 +0200

    feat: add translate use case

---
+++ src/TranslateUseCase.ts
@@ -0,0 +1,124 @@
+import {
+    CSS,
+    Enum,
+    FreeTextShort,
+    Html,
+    IUseCaseDefinition,
+    IUseCaseInput,
+    IUseCaseInputFieldValue,
+    IUseCaseOutput,
+    IUseCaseOutputPart,
+    IUseCaseOutputPartItem,
+    IWebViewLayoutInput,
+} from '@sdk';
+
+export interface ITranslateInput extends IUseCaseInput {
+    dictionary: IUseCaseInputFieldValue<Enum>;
+    expression: IUseCaseInputFieldValue<FreeTextShort>;
+}
+
+interface IOutputPartItem0 extends IUseCaseOutputPartItem {
+    css: CSS;
+    html: Html;
+}
+
+export interface ITranslateOutput extends IUseCaseOutput {
+    _0: IUseCaseOutputPart<IOutputPartItem0, IWebViewLayoutInput>;
+}
+
+export const TranslateUseCaseDefinition: IUseCaseDefinition<
+    ITranslateInput,
+    ITranslateOutput
+> = {
+    io: {
+        input: {
+            fields: [
+                {
+                    choices: {
+                        static: [
+                            { label: { en: '🇫🇷 🇬🇧' }, value: 'fren' },
+                            { label: { en: '🇫🇷 🇪🇸' }, value: 'fres' },
+                            { label: { en: '🇫🇷 🇩🇪' }, value: 'frde' },
+                            { label: { en: '🇬🇧 🇫🇷' }, value: 'enfr' },
+                            { label: { en: '🇪🇸 🇫🇷' }, value: 'esfr' },
+                        ],
+                    },
+                    key: 'dictionary',
+                    type: {
+                        code: 'Enum',
+                        examples: ['enit'],
+                    },
+                },
+                {
+                    key: 'expression',
+                    type: {
+                        code: 'FreeTextShort',
+                        examples: ['Radis'],
+                    },
+                },
+            ],
+        },
+        output: {
+            parts: [
+                {
+                    fields: [
+                        {
+                            key: 'css',
+                            type: {
+                                code: 'CSS',
+                            },
+                        },
+                        {
+                            key: 'html',
+                            type: {
+                                code: 'Html',
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    lifecycle: {
+        client: {
+            main: async (useCase, { webBrowser, uuidGenerator }) => {
+                const dictionary = useCase.requireFromInput<Enum>('dictionary');
+                const expression =
+                    useCase.requireFromInput<FreeTextShort>('expression');
+
+                const output: ITranslateOutput = {
+                    _0: {
+                        items: [],
+                        layout: {
+                            get: (item) => ({
+                                css: item.css,
+                                html: item.html,
+                            }),
+                            type: 'WebView',
+                        },
+                    },
+                };
+
+                const response = await webBrowser.open(
+                    `https://www.wordreference.com/${dictionary}/${expression}`,
+                    '.WRD',
+                );
+                output._0.items.push({
+                    css: '',
+                    html: response || 'Nothing found',
+                    id: uuidGenerator.v4(),
+                });
+
+                return output;
+            },
+        },
+    },
+    metadata: {
+        action: 'Translate',
+        icon: 'trail-sign-outline',
+        label: {
+            en: 'Translate',
+        },
+        name: 'Translate',
+    },
+};
